e6fbbdc0b2bbe917b7d95d15263046897e472c3b
Stephan Niendorf <stephan@stephanmitph.com>
2023-03-14 16:01:15 +0100

    gar nix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import './App.css';
+import useLocalStorageState from "use-local-storage-state";
 import Form from './components/Form/index';
 import List from './components/List';
 import { useState } from 'react';
+import { useEffect } from "react";
 import {uid} from 'uid'
 
 const initialActivities =[
@@ -14,18 +16,59 @@ const initialActivities =[
 ]
 
 function App() {
-  const [activities, setActivities]= useState(initialActivities);
 
-const filteredActivities = activities.filter((activity) => activity.isForGoodWeather === "on")
-console.log(filteredActivities)
+    const [activities, setActivities] = useLocalStorageState("activities", {
+      defaultValue: initialActivities,
+    });
+    const [weather, setWeather] = useLocalStorageState("weather", {
+      defaultValue: null,
+    });
+
+
 
  function handleAddActivity(newActivity){
     setActivities([{id: uid(), ...newActivity}, ...activities]);
   }
-  //console.log(activities)
+
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const response = await fetch(
+          "https://example-apis.vercel.app/api/weather"
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setWeather(data);
+        } else {
+          console.error("Bad server response");
+        }
+        console.log(weather)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    
+    fetchWeather();
+    const timer = setInterval(() => {
+      fetchWeather();
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [weather]);
+
+  const filteredActivities = activities.filter((activity) => activity.isForGoodWeather === "on");
+
+
+  
   return (
     <div className="App">
-      <List activities={filteredActivities}/>
+      <h1 className="app__heading">
+            <span>{weather.condition}</span>
+            <span>{weather.temperature} °C</span>
+          </h1>
+      <List activities={filteredActivities} isGoodWeather={weather?.isGoodWeather}/>
       <Form onAddActivity={handleAddActivity}/>
     </div>
   );
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,11 +1,18 @@
 import './List.css'
 
-export default function List({activities}){
+export default function List({activities, isGoodWeather}){
     return(
-        <ul>
-            {activities.map((activity)=>(
-                <li key={activity.id}>{activity.name}</li>
-            ))}
-        </ul>
+        <>
+            <h2>
+            {isGoodWeather
+              ? "The weather is awesome! Go outside and:"
+              : "Bad weather outside! Here's what you can do now:"}
+            </h2>
+            <ul>
+                {activities.map((activity)=>(
+                    <li key={activity.id}>{activity.name}</li>
+                ))}
+           </ul>
+        </>
     );
 }
